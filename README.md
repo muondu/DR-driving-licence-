010ce9d49b01db1643d44cdf0f1d5fda5333e03a
muondu <munenemuondu@gmail.com>
2020-04-07 12:24:33 +0300

    This projcet checks if you can get a DR(driving licence)

---
+++ README.md
@@ -0,0 +1,2 @@
+# My project
+This project checks if you can have and DR(driving licence). You input your age and if you are 18+, it will say you can get an DR. If you are not at that age or above, it says you can not have an DR.
---
+++ pr1.py
@@ -0,0 +1,12 @@
+def main():
+	global z
+	z = int(input("Enter your age:  "))
+def parts():
+	main()
+	if z > 18:
+		print("You can have a driving licence")
+	elif a == 18:
+		print("You can have a driving licence")
+	else:
+		print("You can't have a driving licence")
+parts()
